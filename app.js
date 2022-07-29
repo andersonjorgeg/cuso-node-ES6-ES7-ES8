f48b07ade017386af02c6ca00048a404bacf0134
Anderson Jorge <anderson.jorge33@gmail.com>
2022-07-29 20:15:30 -0300

    Aula - Constantes

---
+++ app.js
@@ -0,0 +1,9 @@
+// const - constante é uma variável que não pode ser alterada
+const nome = 'Anderson';
+console.log(nome); // Anderson
+
+nome = 'João';
+console.log(nome); // Erro de tipo não capturado: atribuição à variável constante.
+
+// const idade; // Erro de sintaxe não capturado: inicializador ausente na declaração const
+// console.log(idade);
