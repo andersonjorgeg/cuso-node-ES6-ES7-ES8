84dd606ebf00a96603e0658810caf62acb37be60
Anderson Jorge <anderson.jorge33@gmail.com>
2022-08-15 20:24:50 -0300

    Aula - Operador Spread

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-// JSON - encurtados
+// operador spread (...) - espalha os elementos de um array ou objeto em outro array ou objeto
 
 var nome = "Anderson Jorge";
 var idade = 37;
-var empresa = "Google";
 
-// JSON - completo
-var user = {
-  nome: nome,
-  idade: idade,
-  empresa: empresa
+var empresa =  {
+  nomeEmpresa: "Google",
+  cidade: "São Paulo",
+  site: "www.google.com.br",
+  email: "anderson.jorge33@gmail.com"
 }
 
-// JSON - reduzido
+// antes do ES6
+var user = {
+  nome,
+  idade,
+  nomeEmpresa: empresa.nomeEmpresa,
+  cidade: empresa.cidade,
+  site: empresa.site,
+  email: empresa.email
+}
+
+// usando o operador spread (...)
 var user2 = {
   nome,
   idade,
-  empresa
+  ...empresa
 }
 
 console.log(user);
 console.log(user2);
+
+// array
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var array2 = [...array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+console.log(array2);
