5da88071407253735464988575e911a0993c8e14
Anderson Jorge <anderson.jorge33@gmail.com>
2022-08-23 19:27:13 -0300

    Aula - desestruturação

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-// operador spread (...) - espalha os elementos de um array ou objeto em outro array ou objeto
+// desestruturação
 
-var nome = "Anderson Jorge";
-var idade = 37;
-
-var empresa =  {
-  nomeEmpresa: "Google",
-  cidade: "São Paulo",
-  site: "www.google.com.br",
-  email: "anderson.jorge33@gmail.com"
+var user =  {
+  nome: "Anderson Jorge",
+  prof: "Ajudante de mecânica de manutenção",
+  empresa: "ICN",
+  curso: "Técnico em mecânica"
 }
 
-// antes do ES6
-var user = {
-  nome,
-  idade,
-  nomeEmpresa: empresa.nomeEmpresa,
-  cidade: empresa.cidade,
-  site: empresa.site,
-  email: empresa.email
-}
+var { nome, prof, empresa } = user; // destruct
+var curso = user.curso; // antigo
 
-// usando o operador spread (...)
-var user2 = {
-  nome,
-  idade,
-  ...empresa
-}
-
-console.log(user);
-console.log(user2);
-
-// array
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var array2 = [...array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-console.log(array2);
+console.log(nome);
+console.log(prof);
+console.log(empresa);
+console.log(curso);
