106caa00350a2460e3588792777a9263e7af6ac6
Anderson Jorge <anderson.jorge33@gmail.com>
2022-08-23 19:58:01 -0300

    Aula - arrow function

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-// desestruturação
 
-var user =  {
-  nome: "Anderson Jorge",
-  prof: "Ajudante de mecânica de manutenção",
-  empresa: "ICN",
-  curso: "Técnico em mecânica"
+function soma(a, b) {
+  console.log(a + b);
 }
 
-var { nome, prof, empresa } = user; // destruct
-var curso = user.curso; // antigo
+let mult = function(a, b) {
+  console.log(a * b);
+}
 
-console.log(nome);
-console.log(prof);
-console.log(empresa);
-console.log(curso);
+// arrow functions
+
+let mult2 = (a, b, c) => {
+  console.log(a * b * c);
+}
+
+// arrow function com um parâmetro
+let mult4 = a => {
+  console.log(a * 2);
+}
+
+// arrow function só com uma linha
+let mult5 = a => console.log(a * 3);
+
+// arrow function com retorno de forma convencional
+let soma2 = (a, b) => {
+  return a + b;
+}
+
+let resultado = soma2(3, 5);
+
+console.log(resultado);
+
+// arrow function com retorno de forma reduzida
+let soma3 = (a, b) => a + b;
+console.log(soma3(4, 5));
